4da01898afe8a4811b21f95fd9a4a805e5ebff26
Sergei_Zheleznov <rusergei2010@gmail.com>
2018-08-02 10:30:20 +0300

    - add ThreadPools examples and semaphores

---
+++ Lecture 11 (FixedSizeThreadPool, CachedThreadPool, Scheaduled, ForkJoinPool, Semaphore, Locks)/src/main/java/prepare/RaceConditionForkJoinPoolShutDownPool.java
@@ -0,0 +1,54 @@
+package prepare;
+
+import prepare.util.Util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class RaceConditionForkJoinPoolShutDownPool {
+
+
+    public static Callable<Integer> callable(int result, int sleep) {
+        return () -> {
+            Thread.sleep(sleep);
+            return result;
+        };
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        ExecutorService service = Executors.newWorkStealingPool();
+
+        List<Callable<Integer>> callable = Arrays.asList(
+                callable(1, 100),
+                callable(2, 0 /*10*/), // play around
+                callable(3, 100)
+        );
+
+
+        int result = service.invokeAny(callable); // returns the first task being completed
+
+        // !!! If threads are started then we need to enforce it and shut it down
+        //
+        System.out.println("Counter = " + result);
+        System.out.println("The process is started and completed");
+
+        putDown(service, 2);
+    }
+
+    /**
+     * Refer to ExecutorService shutdown recommendations in docs
+     */
+    private static void putDown(ExecutorService service, int delay) throws InterruptedException {
+        if (!service.awaitTermination(delay, TimeUnit.SECONDS)) {
+            service.shutdownNow();
+        }
+    }
+}
