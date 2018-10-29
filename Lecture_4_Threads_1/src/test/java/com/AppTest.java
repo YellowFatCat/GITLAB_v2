7a303975df70103bdbdc974aca1f29b5e9986774
S.Zheleznov <S.Zheleznov@thomsonreuters.com>
2018-10-29 18:53:31 +0300

    - added tests

--- Lecture_4_Threads_1/src/test/java/com/AppTest.java
+++ Lecture_4_Threads_1/src/test/java/com/AppTest.java
@@ -15,6 +15,10 @@ public class AppTest {
         }
     }
 
+    /**
+     * TODO: Fix the test. Employ join()
+     * @throws InterruptedException
+     */
     @Test
     public void testThread() throws InterruptedException {
 
@@ -25,9 +29,6 @@ public class AppTest {
         Thread thread2 = createThread(myRunnable);
         thread1.start();
         thread2.start();
-
-        thread1.join();
-        thread2.join();
 
         assertEquals(2, myRunnable.counter);
     }
